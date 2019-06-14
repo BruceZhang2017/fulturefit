5ac8a949701a141e40535b41fc9d08e31c1d135c
bruce.zhang <315082431@qq.com>
2019-06-14 20:18:54 +0800

    初始代码

---
+++ FultureFit/FultureFitTests/FultureFitTests.swift
@@ -0,0 +1,38 @@
+//
+// Copyright © 2015-2018 Anker Innovations Technology Limited All Rights Reserved.
+// The program and materials is not free. Without our permission, any use, including but not limited to reproduction, retransmission, communication, display, mirror, download, modification, is expressly prohibited. Otherwise, it will be pursued for legal liability.
+// 
+//  FultureFitTests.swift
+//  FultureFitTests
+//
+//  Created by ANKER on 2019/6/14.
+//  Copyright © 2019 PDP-ACC. All rights reserved.
+//
+	
+
+import XCTest
+@testable import FultureFit
+
+class FultureFitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ FultureFit/FultureFitUITests/FultureFitUITests.swift
@@ -0,0 +1,38 @@
+//
+// Copyright © 2015-2018 Anker Innovations Technology Limited All Rights Reserved.
+// The program and materials is not free. Without our permission, any use, including but not limited to reproduction, retransmission, communication, display, mirror, download, modification, is expressly prohibited. Otherwise, it will be pursued for legal liability.
+// 
+//  FultureFitUITests.swift
+//  FultureFitUITests
+//
+//  Created by ANKER on 2019/6/14.
+//  Copyright © 2019 PDP-ACC. All rights reserved.
+//
+	
+
+import XCTest
+
+class FultureFitUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
