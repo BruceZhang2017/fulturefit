112d13e8b71c2ef6753c06e7c8df56d8446e297f
bruce.zhang <315082431@qq.com>
2019-06-14 21:50:48 +0800

    新增登录界面

---
+++ FultureFit/FultureFit/View/User/LoginViewController.swift
@@ -0,0 +1,45 @@
+//
+// Copyright © 2015-2018 Anker Innovations Technology Limited All Rights Reserved.
+// The program and materials is not free. Without our permission, any use, including but not limited to reproduction, retransmission, communication, display, mirror, download, modification, is expressly prohibited. Otherwise, it will be pursued for legal liability.
+// 
+//  ViewController.swift
+//  FultureFit
+//
+//  Created by ANKER on 2019/6/14.
+//  Copyright © 2019 PDP-ACC. All rights reserved.
+//
+	
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginButton: UIButton! // 登录按钮
+    @IBOutlet weak var phoneTextField: UITextField! // 手机号输入框
+    @IBOutlet weak var pwdTextField: UITextField! // 密码输入框
+    @IBOutlet weak var registerButton: UIButton! // 注册按钮
+    @IBOutlet weak var forgetButton: UIButton! // 忘记密码
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - Action
+    
+    // 忘记密码
+    @IBAction func forgetPWD(_ sender: Any) {
+        
+    }
+    
+    // 注册账户
+    @IBAction func registerUser(_ sender: Any) {
+        
+    }
+    
+    // 登录账户
+    @IBAction func loginUser(_ sender: Any) {
+        
+    }
+}
+
--- FultureFit/FultureFit/View/User/RegisterViewController.swift
+++ FultureFit/FultureFit/View/User/RegisterViewController.swift
@@ -2,7 +2,7 @@
 // Copyright © 2015-2018 Anker Innovations Technology Limited All Rights Reserved.
 // The program and materials is not free. Without our permission, any use, including but not limited to reproduction, retransmission, communication, display, mirror, download, modification, is expressly prohibited. Otherwise, it will be pursued for legal liability.
 // 
-//  ViewController.swift
+//  RegisterViewController.swift
 //  FultureFit
 //
 //  Created by ANKER on 2019/6/14.
@@ -12,13 +12,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RegisterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
